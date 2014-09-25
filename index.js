6c6d46aa46131787196e41e4b4e263fc42b80e70
chirag <jain_chirag04@yahoo.com>
2014-09-25 19:39:29 +0530

    write to manifest.json

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var path = require('path');
+var fs = require('fs');
 var crypto = require('crypto');
 var through2 = require('through2');
 var gutil = require('gulp-util');
 var PluginError = gutil.PluginError;
 
-module.exports = function () {
+module.exports = function (manifest_path) {
+  var files = { assets: {} };
   return through2.obj(function(file, enc, cb) {
 
     if (file.isNull()) {
@@ -17,15 +19,21 @@ module.exports = function () {
 
     var src = file.contents.toString('utf8');
     file.hash = crypto.createHash('md5').update(src).digest('hex');
-    
-    var basename = path.basename(file.path);
+
+    var base_path = file.path.replace(file.base, '');
+
+    var basename = path.basename(base_path);
 
     var new_basename = basename.split('.');
         new_basename[0] = new_basename[0] + "-" + file.hash;
         new_basename = new_basename.join('.');
 
     file.path = file.path.replace(basename, new_basename);
+    files.assets[base_path] = base_path.replace(basename, new_basename);
     cb(null, file);
 
+  })
+  .on('end', function () {
+    fs.writeFileSync(path.resolve(manifest_path), JSON.stringify(files, null, 4));
   });
 };
